f814a31fa90c76d8d72d282c82121e4de7b3b190
vladvasile00 <vlad.vasile00@e-uvt.ro>
2021-09-28 08:40:38 +0300

    Lab 1 v1

---
+++ src/main.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+        Book discoTitanic = new Book("Disco Titanic");
+        discoTitanic.createNewParagraph("Paragraph 1");
+        discoTitanic.createNewParagraph("Paragraph 2");
+        discoTitanic.createNewParagraph("Paragraph 3");
+        discoTitanic.createNewImage("Image 1");
+        discoTitanic.createNewParagraph("Paragraph 4");
+        discoTitanic.createNewTable("Table 1");
+        discoTitanic.createNewParagraph("Paragraph 5");
+        discoTitanic.print();
+        }
